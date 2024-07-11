97756485dea2b4e8a93d04d22644d39c24006210
Muneeb Ahmed <165062348+muneebmoon@users.noreply.github.com>
2024-07-11 17:38:35 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace TempratureConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputTemp = new System.Windows.Forms.TextBox();
+            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb2 = new System.Windows.Forms.ComboBox();
+            this.outtxt = new System.Windows.Forms.TextBox();
+            this.Convertbtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(292, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Temprature Converter";
+            // 
+            // inputTemp
+            // 
+            this.inputTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTemp.Location = new System.Drawing.Point(270, 92);
+            this.inputTemp.Name = "inputTemp";
+            this.inputTemp.Size = new System.Drawing.Size(234, 35);
+            this.inputTemp.TabIndex = 1;
+            // 
+            // cb1
+            // 
+            this.cb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb1.FormattingEnabled = true;
+            this.cb1.Location = new System.Drawing.Point(643, 92);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(132, 37);
+            this.cb1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(76, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter Temprature";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(520, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Select Unit";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(265, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Targeted Unit";
+            // 
+            // cb2
+            // 
+            this.cb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb2.FormattingEnabled = true;
+            this.cb2.Location = new System.Drawing.Point(411, 176);
+            this.cb2.Name = "cb2";
+            this.cb2.Size = new System.Drawing.Size(149, 37);
+            this.cb2.TabIndex = 7;
+            // 
+            // outtxt
+            // 
+            this.outtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outtxt.Location = new System.Drawing.Point(323, 324);
+            this.outtxt.Name = "outtxt";
+            this.outtxt.ReadOnly = true;
+            this.outtxt.Size = new System.Drawing.Size(320, 35);
+            this.outtxt.TabIndex = 8;
+            // 
+            // Convertbtn
+            // 
+            this.Convertbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Convertbtn.Location = new System.Drawing.Point(411, 248);
+            this.Convertbtn.Name = "Convertbtn";
+            this.Convertbtn.Size = new System.Drawing.Size(106, 52);
+            this.Convertbtn.TabIndex = 9;
+            this.Convertbtn.Text = "Convert";
+            this.Convertbtn.UseVisualStyleBackColor = true;
+            this.Convertbtn.Click += new System.EventHandler(this.Convertbtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(223, 330);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 400);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Convertbtn);
+            this.Controls.Add(this.outtxt);
+            this.Controls.Add(this.cb2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cb1);
+            this.Controls.Add(this.inputTemp);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Temprature Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputTemp;
+        private System.Windows.Forms.ComboBox cb1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cb2;
+        private System.Windows.Forms.TextBox outtxt;
+        private System.Windows.Forms.Button Convertbtn;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TempratureConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cb1.Items.Add("Celcius");
+            cb1.Items.Add("Fahrenheit");
+            cb1.Items.Add("Kelvin");
+            cb2.Items.Add("Celcius");
+            cb2.Items.Add("Fahrenheit");
+            cb2.Items.Add("Kelvin");
+        }
+
+        private void Convertbtn_Click(object sender, EventArgs e)
+        {
+            if (inputTemp.Text == "" || cb1.Text == "" || cb2.Text == "")
+            {
+                MessageBox.Show("All Above fields are required! Please Fill All fields");
+            }
+            else
+            {
+                if(IsValidInput(inputTemp.Text))
+                {
+                    double input = Convert.ToDouble(inputTemp.Text);
+                    Conversion(input, cb1.Text, cb2.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Input must be a number");
+                }
+                
+            }
+        }
+
+        private bool IsValidInput(string input)
+        {
+            return double.TryParse(input, out _);
+        }
+
+        private void Conversion(double input, string unit, string targetUnit)
+        {
+            if (unit == targetUnit)
+            {
+                MessageBox.Show("Targeted Unit must be different from input Unit");
+                outtxt.Text = "";
+            }
+            else
+            {
+                if (unit == "Celcius" && targetUnit == "Fahrenheit")
+                {
+                    double output = ((input * (9.0 / 5.0)) + 32);
+                    outtxt.Text = output.ToString() + " F";
+                }
+                if (unit == "Fahrenheit" && targetUnit == "Celcius")
+                {
+                    double output = ((input - 32) * (5.0 / 9.0));
+                    outtxt.Text = output.ToString() + " C";
+                }
+
+                if (unit == "Celcius" && targetUnit == "Kelvin")
+                {
+                    double output = input + 273.15;
+                    outtxt.Text = output.ToString() + " K";
+                }
+
+                if (unit == "Fahrenheit" && targetUnit == "Kelvin")
+                {
+                    double output = (input - 32) * (5.0 / 9.0) + 273.15;
+                    outtxt.Text = output.ToString() + " K";
+                }
+
+                if (unit == "Kelvin" && targetUnit == "Celcius")
+                {
+                    double output = (input - 273.15);
+                    outtxt.Text = output.ToString() + " C";
+                }
+
+                if (unit == "Kelvin" && targetUnit == "Fahrenheit")
+                {
+                    double output = (input - 273.15) * (9.0 / 5.0) + 32;
+                    outtxt.Text = output.ToString() + " F";
+                }
+
+            }
+
+        }
+    }
+}
